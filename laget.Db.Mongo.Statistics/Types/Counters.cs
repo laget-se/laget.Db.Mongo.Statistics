ad81c354dd2a3898fbe85fb273a85f8c6dd2738f
Erik Hennerfors <erik.hennerfors@obscured.se>
2020-12-18 10:20:45 +0100

    Added type Counters to default types

---
+++ laget.Db.Mongo.Statistics/Types/Counters.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace laget.Db.Mongo.Statistics.Types
+{
+    public class Counters : Entity
+    {
+        [BsonElement("date"), BsonDateTimeOptions(DateOnly = true)]
+        public DateTime Date { get; set; } = DateTime.Now.Date;
+    }
+}
